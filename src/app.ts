aabff037413b8a469360fb379628ca0495547a17
StephenOG <ogunderostephen@gmail.com>
2022-11-15 00:05:12 +0100

    added mysql port to config

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,7 @@
 import express, {Request,Response,Application} from 'express';
 import cors from 'cors'
-import { db } from "./db/config";
-// import userRouter from './routers/user'
 import { userRouter } from './user/users.routers';
 import { transactionRouter } from './transactions/transactions.routers';
-// import userRouter from './routers/user'
-// import transactionRouter from './routers/transaction'
-
-console.log(db.name)
 
 const app:Application = express();
 
@@ -16,8 +10,5 @@ app.use(express.json())
 
 app.use("/api/users", userRouter)
 app.use("/api/transactions", transactionRouter)
-
-// app.use(userRouter)
-// app.use(transactionRouter)
 
 export default app
