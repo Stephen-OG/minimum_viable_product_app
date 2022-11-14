02a89c1a8c4c149197f09c426f09e6d0f3bef0f9
StephenOG <ogunderostephen@gmail.com>
2022-11-15 00:52:47 +0100

    deleted router folder

--- src/routers/transaction.ts
+++
@@ -1,9 +0,0 @@
-import express,{Request, Response, Router } from "express";
-
-const router:Router = express();
-
-router.get("/transaction", (req:Request, res:Response):void => {
-    res.send("Hello transaction")
-  });
-
-export default router
