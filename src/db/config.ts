93dad713876ff1ec15a18ab3be09c320839768f5
StephenOG <ogunderostephen@gmail.com>
2022-11-14 23:59:28 +0100

    made changes to the config file

--- src/db/config.ts
+++ src/db/config.ts
@@ -19,7 +19,8 @@ const config = knex({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB
+    database: process.env.MYSQL_DB,
+    port: Number(process.env.MYSQL_PORT)
   },
   useNullAsDefault: true
 })
